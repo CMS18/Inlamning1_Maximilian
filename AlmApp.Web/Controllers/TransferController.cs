701e8f94b077d7b3e726489d6d9ec68f20dc9f6d
Gabriel Jealmo <gabrieljealmo@gmail.com>
2019-11-14 12:06:03 +0100

    Added transfer feature to bankrepository with controller and view. Also made unittests for the transfer feature. Added menu item for the transfer view.

---
+++ AlmApp.Web/Controllers/TransferController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlmApp.Web.Data;
+using AlmApp.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlmApp.Web.Controllers
+{
+    public class TransferController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Transfer(TransferViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["msg"] = BankRepository.Transfer(model.AccountFrom, model.AccountTo, model.Amount);
+                return View("Index");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ AlmApp.Web/Models/ViewModels/TransferViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlmApp.Web.Models.ViewModels
+{
+    public class TransferViewModel
+    {
+        [Required]
+        public int AccountFrom { get; set; }
+        [Required]
+        public int AccountTo { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+    }
+}
